60a117b2dc5f69c2dbf360a51c8efd3c9dc9a2df
Lizhou Sha <lizhou@lsha.net>
2024-12-24 16:38:32 -0600

    Day 1: Initial solution

---
+++ src/01.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# ruff: noqa: F401
+import collections
+import functools
+import io
+import itertools
+import operator as op
+import re
+import timeit
+
+
+import numpy as np
+import aocd
+
+YEAR = 2024
+DAY = 1
+
+
+def sim(a, b):
+    counter = collections.Counter(b)
+    return sum(i * counter[i] for i in a)
+
+
+def main():
+    data = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+"""
+    data = aocd.get_data(day=DAY, year=YEAR)
+    inlist = np.array([tuple(map(int, l.split())) for l in data.split('\n') if l])
+
+    # print(np.sort(inlist, axis=0))
+    answer = np.sum(np.abs(np.diff(np.sort(inlist, axis=0), axis=1)))
+    print(answer)
+    # aocd.submit(answer, part='a', day=DAY, year=YEAR)
+
+    answer = sim(inlist[:, 0], inlist[:, 1])
+    print(answer)
+    aocd.submit(answer, part='b', day=DAY, year=YEAR)
+
+
+if __name__ == '__main__':
+    main()
