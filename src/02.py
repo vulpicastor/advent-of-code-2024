37f5beaa9e209984919bd7d845d41ee079a9c992
Lizhou Sha <lizhou@lsha.net>
2024-12-26 11:01:30 -0600

    Day 2: Cleanup

--- src/02.py
+++ src/02.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 
-# ruff: noqa: F401
-import collections
-import functools
-import io
-import itertools
-import operator as op
-import re
-import timeit
-
 import numpy as np
 import aocd
 
@@ -28,95 +19,39 @@ def safe(l):
 
 def safe2(l):
     diff = np.diff(l)
-    # abs_diff = np.abs(diff)
-    # if np.all(diff >= 0) or np.all(diff <= 0):
-    #     print('Condition: Monotonic')
-    #     if np.any(abs_diff >= 3):
-    #         print('Return: Gap too big')
-    #         return False
-    #     if np.sum(abs_diff < 1) >= 1:
-    #         print('Return: Too many return')
-    #         return False
-    #     print('Return: Safe')
-    #     return True
-        # diff_in_bound = (abs_diff >= 1) & (abs_diff <= 3)
-        # if np.all(diff_in_bound):
-        #     return True
-        # out_bound = ~diff_in_bound
-        # total_out_bound = np.sum(out_bound)
-        # if total_out_bound > 2:
-        #     return False
-        # where_out_bound = np.where(out_bound)
-        # if total_out_bound == 2 and np.diff(where_out_bound) > 1:
-        #         return False
-        # fix = where_out_bound[0]
-        # if fix >= len(diff) - 1:
-        #     return False
-        # fix_diff = np.sum(diff[fix:fix + 2])
-        # if 1 <= fix_diff <= 3:
-        #     return True
-        # return
+    # Identify safe pairs.
     safe_diff_inc = (1 <= diff) & (diff <= 3)
     safe_diff_dec = (-3 <= diff) & (diff <= -1)
     len_diff = len(diff)
     if ((num_safe_diff_inc := np.sum(safe_diff_inc)) == len_diff
         or (num_safe_diff_dec := np.sum(safe_diff_dec)) == len_diff):
-        # print('Return SAFE: all safe')
+        # All pairs are safe.
         return True
-    print(l)
-    # unsafe_inc = num_safe_diff_inc < len_diff - 1
-    # unsafe_dec = np.sum(safe_diff_dec) < len_diff - 1
     if num_safe_diff_inc < len_diff - 2 and num_safe_diff_dec < len_diff - 2:
-        print('Return UNSAFE: Too many unsafe pairs')
+        # Impossible to resolve > 2 unsafe pairs by removing one number.
         return False
     if num_safe_diff_dec > num_safe_diff_inc:
-        print('Condition: Mostly decreasing, inverting')
+        # Coerce the sequence to be increasing.
         diff = -diff
         idx, = np.where(~safe_diff_dec)
     else:
         idx, = np.where(~safe_diff_inc)
-    # elif (num_lt_zero := np.sum(diff < 0)) > 1 or (num_gt_zero := np.sum(diff > 0)) > 1:
-    #     print('Condition Return: Too many non-monotonic pairs')
-    #     return False
-    # if num_gt_zero == 1:
-    #     print('Condition: Mostly decreasing, inverting')
-    #     diff = -diff
-    # idx = np.where(diff < 0)
-    print(f'Index of unsafe: {idx}')
     if len(idx) == 2:
         if idx[1] - idx[0] > 1:
-            print('Return UNSAFE: Unsafe pairs not adjacent')
+            # Non-adjacent unsafe pairs cannot be resolved.
             return False
         if (1 <= diff[idx[0]] + diff[idx[1]] <= 3):
-            print('Return SAFE: Unsafe pairs merged')
+            # Removing one number merges unsafe pairs into a safe one.
             return True
-        print('Return UNSAFE: Unsafe pairs cannot be merged')
         return False
-        # idx = idx[0]
     if idx == len(diff) - 1 or idx == 0:
-        print('Return SAFE: only first or last pair unsafe')
-        # Last item can always be removed.
+        # First or last item can always be removed to make a sequence safe.
         return True
     if (1 <= diff[idx] + diff[idx + 1] <= 3
         or 1 <= diff[idx - 1] + diff[idx] <= 3):
-        # or 1 <= diff[idx + 1] + diff[idx + 2] <= 3):
-        print('Return SAFE: Removing one element restores safety')
+        # Removing one number resolves the unsafe pair.
         return True
-    print('Return UNSAFE: All checks failed')
     return False
-
-
-def safe_scan(l):
-    unsafe = False
-    sign = 0
-    for i, (p, n) in enumerate(itertools.pairwise(l)):
-        diff = n - p
-        if sign == 0:
-            sign = np.sign(diff)
-        else:
-            pass
-        if abs(diff) > 0:
-            pass
 
 
 def main():
@@ -131,8 +66,8 @@ def main():
     inlist = [np.array([int(i) for i in l.split()]) for l in data.split('\n') if l]
 
     answer = [safe(l) for l in inlist]
-    # print(answer)
-    # aocd.submit(np.sum(answer), part='a', day=DAY, year=YEAR)
+    print(answer)
+    aocd.submit(np.sum(answer), part='a', day=DAY, year=YEAR)
 
     answer = [safe2(l) for l in inlist]
     print(answer)
